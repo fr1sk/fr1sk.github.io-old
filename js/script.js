aab81f2d0e308b587f2ec70592c199a150ee0d2e
fr1sk <fr1sk@live.com>
2016-07-25 13:55:56 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -341,3 +341,16 @@ jQuery(document).ready(function ($) {
 		}
 	});
 });
+
+/***********************
+ * Mobile nav bar hide *
+ * *********************/
+ $(document).ready(function () {
+    $(document).click(function (event) {
+        var clickover = $(event.target);
+        var _opened = $(".navbar-collapse").hasClass("navbar-collapse in");
+        if (_opened === true && !clickover.hasClass("navbar-toggle")) {
+            $("button.navbar-toggle").click();
+        }
+    });
+});
